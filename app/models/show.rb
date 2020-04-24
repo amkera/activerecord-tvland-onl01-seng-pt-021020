ca7cbd674d4ab0b03cc6bb6969401722fc19d82b
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-04-24 19:25:47 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
   
   def actors_list 
     #has an #actors_list method that returns a list of the full names of each actor associated with the show
-    
+    binding.pry 
     
     
   end 
